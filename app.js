f91e9f988c250b96b1d3948416bb700d768ace65
Alexandre Ferreira <ferreiraalexandre468@gmail.com>
2022-05-02 00:40:23 -0300

    Tratamento de erro adicionado para quando seja feita uma requisição para uma rota inexistente, além de outros erros.

--- app.js
+++ app.js
@@ -14,4 +14,18 @@ app.use('/persons', personsRoute);
 app.use('/items', itemsRoute);
 app.use('/markets', marketsRoute);
 
+app.use((req,res,next)=>{
+    const erro = new Error("Cannot find route specified.")
+    erro.status = 404;
+    next(erro)
+})
+
+app.use((error, req, res, next)=>{
+  res.status(error.status || 500)
+  return res.send({
+    erro: {
+        mensagem: error.message
+    }
+  });
+});
 module.exports = app;
