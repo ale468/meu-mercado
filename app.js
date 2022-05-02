505d1239ec583e334320b19eaddebfa304de95a8
Alexandre Ferreira <ferreiraalexandre468@gmail.com>
2022-05-02 00:29:49 -0300

    morgan incluido

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const productsRoute = require("./routes/products");
 const personsRoute = require("./routes/persons");
 const itemsRoute = require("./routes/Items");
 const marketsRoute = require("./routes/markets");
+const morgan = require("morgan");
+
+app.use(morgan('dev'));
 
 app.use('/products', productsRoute);
 app.use('/persons', personsRoute);
