72cb0e3abce56b66e9ac0e223529c8e9b3e1b16a
Alexandre Ferreira <ferreiraalexandre468@gmail.com>
2022-05-07 17:50:58 -0300

    CORS adicionado ao app.js

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Controll-Allow-header",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method == "OPTIONS"){
+      res.header("Acces-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH");
+      return res.status(200).send({});
+    }
+    next();
+});
+
 app.use('/products', productsRoute);
 app.use('/persons', personsRoute);
 app.use('/items', itemsRoute);
