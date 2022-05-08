775018e5e72d871defcdd3f9927aeee188acab21
Alexandre Ferreira <ferreiraalexandre468@gmail.com>
2022-05-07 21:42:41 -0300

    Foram feitas alterações na criação das tabelas. Adicionando a auto incrementação para chaves primárias e exclusão do delele e patch para simplificação do projeto.

--- dtb/creates.sql
+++ dtb/creates.sql
@@ -2,10 +2,10 @@ CREATE DATABASE IF NOT EXISTS  meu_mercado;
 
 USE meu_mercado;
 
-CREATE TABLE markets (market_id INT NOT NULL, market_name VARCHAR(80), address VARCHAR (150), PRIMARY KEY (market_id));
+CREATE TABLE IF NOT EXISTS markets (market_id INT NOT NULL AUTO_INCREMENT, market_name VARCHAR(80), address VARCHAR (150), PRIMARY KEY (market_id));
 
-CREATE TABLE persons (cpf CHAR(11) NOT NULL, full_name VARCHAR(20), login_user VARCHAR(50), pwd_user VARCHAR(50), market_id INT, PRIMARY KEY (cpf),  FOREIGN KEY (market_id) REFERENCES markets(market_id));
+CREATE TABLE IF NOT EXISTS persons (cpf CHAR(11) NOT NULL, full_name VARCHAR(20), login_user VARCHAR(50), pwd_user VARCHAR(50), market_id INT, PRIMARY KEY (cpf),  FOREIGN KEY (market_id) REFERENCES markets(market_id));
 
-CREATE TABLE products (product_id VARCHAR(80) , product_name VARCHAR(50), PRIMARY KEY(product_id));
+CREATE TABLE IF NOT EXISTS products (product_id INT NOT NULL AUTO_INCREMENT, product_name VARCHAR(50), PRIMARY KEY(product_id));
 
-CREATE TABLE items (market_id INT NOT NULL, product_id VARCHAR(80) NOT NULL, item_price FLOAT(5,2), product_picture_path VARCHAR(300), product_description VARCHAR (200), FOREIGN KEY (product_id) REFERENCES products(product_id));
+CREATE TABLE IF NOT EXISTS items (item_id INT NOT NULL AUTO_INCREMENT, market_id INT NOT NULL, product_id INT NOT NULL, item_price FLOAT(5,2), product_picture_path VARCHAR(300), product_description VARCHAR (200), PRIMARY KEY(item_id), FOREIGN KEY (product_id) REFERENCES products(product_id));
--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("../mysql").pool;
 
-router.get('/', (req, res, next) => {
-  res.status(200).send({
-      mensagem: "GET deu certo!"
-  });
-});
 router.get('/*', (req, res, next) => {
   mysql.getConnection((error, conn) => {
     conn.query(
@@ -53,8 +48,8 @@ router.post('/', (req, res, next) => {
 
   mysql.getConnection((error, conn) => {
     conn.query(
-      "INSERT INTO products VALUES (?,?)",
-      [req.body.id, req.body.name],
+      "INSERT INTO products (product_name) VALUES (?)",
+      [req.body.name],
       (error, result, field) => {
         conn.release();
         if(error){
@@ -67,18 +62,6 @@ router.post('/', (req, res, next) => {
           mensagem: "Produto inserido com sucesso!"
         });
     });
-  });
-});
-
-router.patch('/', (req, res, next) => {
-  res.status(201).send({
-      mensagem: "PATCH deu certo",
-  });
-});
-
-router.delete('/', (req, res, next) => {
-  res.status(201).send({
-      mensagem: "DELETE deu certo"
   });
 });
 
