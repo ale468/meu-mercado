50cb166eb3676e53fdd8702f742acdddb474738b
Alexandre Ferreira <ferreiraalexandre468@gmail.com>
2022-05-08 02:19:08 -0300

    Rota items agora com post otimizado

--- routes/Items.js
+++ routes/Items.js
@@ -3,25 +3,69 @@ const router = express.Router();
 const mysql = require("../mysql").pool;
 
 router.get('/', (req, res, next) => {
-  res.status(200).send({
-      mensagem: "GET deu certo"
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "SELECT * FROM items",
+      (error, result, field) => {
+        conn.release();
+        if(error){
+          res.status(500).send({
+            error: error,
+            response: null
+          })
+        }
+        res.status(200).send({
+          message: "Get concluído!",
+          result: result,
+        });
+      }
+    );
   });
 });
 
 router.post('/', (req, res, next) => {
 
-  const item = {
-    marketId: req.body.name,
-    productId: req.body.price,
-    itemPrice: req.body.itemPrice,
-    productPicturePath: req.body.productPicturePath,
-    productDescription: req.body.productDescription
-  };
-
-  res.status(201).send({
-      mensagem: "POST deu certo",
-      result: item
-  });
+  mysql.getConnection((error,conn) => {
+      conn.query(
+        "SELECT product_id FROM products WHERE product_id = ?",
+        req.body.productId,
+        (error, result, field) => {
+          if(result.length == 0){
+            res.status(400).send({
+              message: "product_id not found! Please try again."
+            });
+          }else{
+            conn.query(
+              "SELECT market_id FROM markets WHERE market_id = ?",
+              req.body.marketId,
+              (error, result, field) => {
+                if(result.length == 0){
+                  res.status(400).send({
+                    message: "market_id not found! Please try again."
+                  });
+                }else{
+                  conn.query(
+                    "INSERT INTO items (market_id, product_id, item_price, product_picture_path, product_description) VALUES (?,?,?,?,?)",
+                    [req.body.marketId, req.body.productId, req.body.item_price, req.body.productPicturePath, req.body.productDescription],
+                    (error, result, field) => {
+                      if(error){
+                        res.status(500).send({
+                          error: error,
+                          response: null
+                        })
+                      }
+                      res.status(201).send({
+                        mensagem: "insertion concluded!",
+                      });
+                    }
+                  );
+                }
+              }
+            );
+          }
+        }
+      );
+  })
 });
 
 router.patch('/', (req, res, next) => {
